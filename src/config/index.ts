2758f94ccf1e4765a4fa4f3729458b330fb2be1e
eric_wang <jadeface.scholar@gmail.com>
2022-06-19 19:07:25 +0800

    fix: fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,6 @@ const YAML_CONFIG_FILENAME = 'config.yaml';
 export default () => {
   return yaml.load(
     // 同步读取配置文件，防止 service 服务的阻塞
-    readFileSync(join(__dirname, `config/${YAML_CONFIG_FILENAME}`), 'utf8'),
+    readFileSync(join(__dirname, `${YAML_CONFIG_FILENAME}`), 'utf8'),
   ) as Record<string, any>;
 };
