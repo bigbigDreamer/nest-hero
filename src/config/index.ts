175a0eea5d7c30f4fd4c3e756f272027606053b2
eric_wang <jadeface.scholar@gmail.com>
2022-06-19 21:54:47 +0800

    feat: add process.cwd

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,9 @@ const YAML_CONFIG_FILENAME = 'config.yaml';
 export default () => {
   return yaml.load(
     // 同步读取配置文件，防止 service 服务的阻塞
-    readFileSync(join(__dirname, `${YAML_CONFIG_FILENAME}`), 'utf8'),
+    readFileSync(
+      join(process.cwd(), `./config/${YAML_CONFIG_FILENAME}`),
+      'utf8',
+    ),
   ) as Record<string, any>;
 };
